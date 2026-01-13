13fd1b4084006fec9034f94dfbe1002e3e147784
Wtc-Klm <incomet2004@gmail.com>
2026-01-13 15:59:33 +0700

    start my project

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Exaple app lisening at http://localhost:${port}`);
+});
