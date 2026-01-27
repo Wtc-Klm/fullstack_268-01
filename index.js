306e06fc96a3f88c28f7e7204187dbff8d3039d9
Wtc-Klm <incomet2004@gmail.com>
2026-01-27 13:57:07 +0700

    .

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.send("hello Wuttichai world!");
 });
 
 app.listen(port, () => {
